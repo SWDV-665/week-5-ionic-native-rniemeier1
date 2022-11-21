4580932ed051ccf197abfc1b91fe1bee90258d9f
R. Niemeier <48067061+rniemeier@users.noreply.github.com>
2022-11-20 21:51:17 -0600

    Added SocialSharing and Screenshots

--- groceries/src/app/tab1/tab1.page.ts
+++ groceries/src/app/tab1/tab1.page.ts
@@ -3,6 +3,7 @@ import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
 import {GroceriesServiceService} from '../groceries-service.service';
 import { InputDialogServiceService } from '../input-dialog-service.service';
+import { SocialSharing } from '@awesome-cordova-plugins/social-sharing/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -14,7 +15,7 @@ export class Tab1Page {
   //Page Title
   title = "Grocery List";
 
-  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController, public DataService: GroceriesServiceService, public InputService: InputDialogServiceService) {
+  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController, public DataService: GroceriesServiceService, public InputService: InputDialogServiceService, public socialShare: SocialSharing) {
 
   }
 
@@ -34,17 +35,28 @@ export class Tab1Page {
       (await toast).present();
     }
 
-    async shareItem(item, index){
-      const itemName = item.name;
-     // this.DataService.removeItem(index);
-  
-      const toast = this.toastCtrl.create({
-        message: 'Shared ' + itemName + ".",
-        duration: 3000
-      });
-      (await toast).present();
-    }
+   async shareItem(item, index){
+       let message = "Grocery Item: " + item.name + " Quantity: " + item.quantity;
+       let subject = "Shared via Groceries App";
+       let response = "";
+      
+       try{
+         this.socialShare.share(message, subject);
+         response = "Shared " + item.name + " successfully.";
+       }catch(e)
+       {
+         response = "Error sharing " + item.name + ". ";
+         console.log(e);
+       }
 
+       const toast = this.toastCtrl.create({
+         message: response,
+         duration: 3000
+      });
+       (await toast).present();
+      
+    }
+ 
     editItem(item, index){
       this.InputService.showPrompt(item, index);
     }
